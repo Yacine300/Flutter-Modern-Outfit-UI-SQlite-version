fb05edc4252b91ebcbecbe676442ab893c12d880
Yacine300 <herrouelyacine7@gmail.com>
2024-07-15 01:01:01 +0100

    update main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:four/data_service/db_service.dart';
 import 'package:provider/provider.dart';
-import 'Providers/Carts.dart';
-import 'Providers/Products.dart';
-import 'Screens/Initialisation/initialisation.dart';
-import 'Composant/Route.dart';
+import 'view_models.dart/cart_view_model.dart';
+import 'view_models.dart/product_view_model.dart';
+import 'views/Initialisation_view/initialisation.dart';
+import 'views/components/Route.dart';
 
 void main() {
   runApp(MyApp());
